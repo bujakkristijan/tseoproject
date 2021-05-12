5240d61e2f7b95637ce7ca9fea6991709dfdc084
bujakkristijan <kbujak996@gmail.com>
2021-05-13 00:56:01 +0200

    ruta dodata za studenta

--- frontendAngular/tseo-frontend/src/app/app-routing.module.ts
+++ frontendAngular/tseo-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { StudentListComponent } from './student-list/student-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'students', component: StudentListComponent},
+  {path: '', redirectTo: 'students', pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
